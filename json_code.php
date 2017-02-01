2d72473c54bc0a6edbcef29d8148adc34af27225
ankushkalra8 <ankushkalra8@users.noreply.github.com>
2017-02-01 23:58:25 +0530

    Update json_code.php

--- json_code.php
+++ json_code.php
@@ -3,6 +3,9 @@
 //header('Content-Type: application/json');
 
 /* random comment 2 */
+
+/* random comment new */
+
 require_once('StudentDB.php');
 
 $json_data = json_decode('{"first_name" : "Dale"}');
